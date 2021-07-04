bad91f0a23865ddc71558c99710c5616695e3647
rizatsk <rizatsakmir@gmail.com>
2021-07-04 10:23:16 +0700

    first commit

---
+++ js/portofolio.js
@@ -0,0 +1,49 @@
+// Portofolio
+
+$.getJSON("json/main.json", function(Portofolio){
+    let datas = Portofolio.projects;
+    $.each(datas, function(i, data){
+        $("#card-portofolio").append(`
+        <div class="col-md mb-4">
+          <div class="card">
+            <img class="card-img-top" src="image/`+data.photo+`" alt="Card image cap">
+            <div class="card-body text-center">
+              <h5 class="card-text">`+data.nama+`.</h5>
+              <p class="card-text">`+data.deskripsi.substring(0, 90)+`;...</p>
+              <a href="#" class="btn btn-primary see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="`+ data.id +`">See Detail...</a>
+              </div>
+              </div>
+              </div> `);
+    });
+});
+
+$('#card-portofolio').on('click','.see-detail', function(){
+  let id = $(this).data('id');
+  $.getJSON("json/main.json", function(Portofolio){
+    let datas = Portofolio.projects;
+    $.each(datas, function(i,data){
+      if(data.id == id){
+        $('.modal-title').html(data.nama);
+        $('.modal-body').html(`
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="image/`+data.photo+`" class="img-fluid">
+                </div>
+                <div class="col-md-8">
+                    <ul class="list-group">
+                        <li class="list-group-item"> <h6>`+ data.nama +`</h6> </li>
+                        <li class="list-group-item"> Deksripsi `+data.deskripsiProject+` </li>
+                        <iframe class="embed-responsive-item" height="300" src="`+data.vidio+`" allowfullscreen></iframe>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        `);
+      }
+    })
+  })
+})
+let age = (new Date().getFullYear()) - 2000; 
+$(".age").html(age+` Years`);
+    
---
+++ js/sosmed.js
@@ -0,0 +1,114 @@
+$.ajax({
+    type : "get",
+    url : "https://www.googleapis.com/youtube/v3/channels",
+    datatype : "json",
+    data : {
+        "part" : "snippet,statistics",
+        "id" : "UCgVOfzzDlutepqNEcp1kbJA",
+        "key" : "AIzaSyDxRJVw4-R1-ZEmTJ1CSe-cmniU1qETDJE"
+    },
+    success : function(result){
+        if(result){
+            let profileYT = result.items[0]['snippet']['thumbnails']['medium']['url'];
+            let namaYT = result.items[0]['snippet']['title'];
+            let subscriberYT = result.items[0]['statistics']['subscriberCount'];
+            let viewsYT = result.items[0]['statistics']['viewCount'];
+            let videosYT = result.items[0]['statistics']['videoCount'];
+            $("#youtube").html(`
+            <div class="d-flex align-items-center">
+                <div class="col-md-3">
+                  <img src="`+profileYT+`" alt="" width="100" class="img-thumbnail rounded-circle">
+                </div>
+                <div class="col-md-8 ms-3">
+                  <h5>`+namaYT+`</h5>
+                  <p>`+subscriberYT+` Subscriber, 
+                  `+viewsYT+` Views, 
+                  `+videosYT+` Video.</p>
+                  <div class="g-ytsubscribe" data-channelid="UCgVOfzzDlutepqNEcp1kbJA" data-layout="default" data-count="default"></div>
+                  <script src="https://apis.google.com/js/platform.js"></script>
+                </div>
+            </div>
+          `);
+        }
+    }
+});
+
+$.ajax({
+    type : "get",
+    url : "https://www.googleapis.com/youtube/v3/search",
+    datatype : "json",
+    data :{
+        "key" : "AIzaSyDxRJVw4-R1-ZEmTJ1CSe-cmniU1qETDJE",
+        "channelId" : "UCgVOfzzDlutepqNEcp1kbJA",
+        "maxResults" : "1",
+        "order" : "date"
+    },
+    success : function(result){
+        if(result){
+            let idVideoLatest = result.items[0]['id']['videoId'];
+           $("#youtube").append(`
+           <div class="row mt-4 mb-4">
+              <div class="col-md-12 embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/`+idVideoLatest+`" allowfullscreen></iframe>
+              </div>
+           </div`);
+        }
+    }
+});
+
+$.ajax({
+    type : "get",
+    url : "https://graph.instagram.com/me",
+    datatype : "json",
+    data :{
+        "fields" : "username,media_count,account_type",
+        "access_token" : "IGQVJYV3ROM2FVajdXeTNXQzN4NlJmQ1VHUG5UQzVJbjFRNndjWFRKTnVTUzJYRmlmUnN5Qmo1eVM5UXQ5YkU3c1pNRE9ZASzVWNXhabS1UdjBOc0ZAPS2pLWTFWMGxCMjY2N2lWMGo4N1ZApMjRCMXVUOAZDZD"
+    },
+    success : function(result){
+        if(result){
+            let username = result['username'];
+            let media_count = result['media_count'];
+            $("#instagram").html(`
+            <div class="d-flex align-items-center">
+                <div class="col-md-3 flex-shrink-0">
+                  <img src="image/profile1.png" alt="" width="100" class="img-thumbnail rounded-circle">
+                </div>
+                <div class="col-md-8 flex-grow-1 ms-3">
+                  <h5>Instagram</h5>
+                  <h5>@`+username+`</h5>
+                  <p>120 Followers, `+media_count+` Media</p>
+                  </div>
+            </div>`);
+        }
+    }
+})
+
+$.ajax({
+    type : "get",
+    url : "https://graph.instagram.com/me/media",
+    datatype : "json",
+    data : {
+        'fields' : "timestamp,thumbnail_url,media_url,media_type,caption",
+        'access_token' : "IGQVJYV3ROM2FVajdXeTNXQzN4NlJmQ1VHUG5UQzVJbjFRNndjWFRKTnVTUzJYRmlmUnN5Qmo1eVM5UXQ5YkU3c1pNRE9ZASzVWNXhabS1UdjBOc0ZAPS2pLWTFWMGxCMjY2N2lWMGo4N1ZApMjRCMXVUOAZDZD"
+    },
+    success : function(result){
+        if(result){
+            let image_ig = result['data'];
+            let gambar = '';
+            for(let i = 0; i <= 8; i++){
+                if(image_ig[i]['media_type'] == "IMAGE"){
+                    gambar += `
+                    <div class="instagram-thumbnail">
+                        <img src="`+image_ig[i]['media_url']+`">
+                    </div>`;
+                }
+            }
+            $("#instagram").append(`
+            <div class="row mt-4 mb-4">
+              <div class="col">
+                `+gambar+`
+              </div>
+            </div>`);
+        }
+    }
+})
