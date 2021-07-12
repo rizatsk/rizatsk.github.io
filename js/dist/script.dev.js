e94872c0b3721f657449cfcffb38013f4e21cb4f
rizatsk <rizatsakmir@gmail.com>
2021-07-12 09:48:06 +0700

    memperbaiki yang kurang bagus

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -7,19 +7,25 @@ function topFunction() {
 
 $(document).ready(function ($) {
   $(window).scroll(function () {
-    if ($(document).scrollTop() > 150) {
+    if ($(document).scrollTop() > 80) {
       // Navigation Bar
-      $('.navbar').removeClass('fadeIn');
+      // $('.navbar').removeClass('fadeIn');
+      $('.navbar').removeClass('bg-abuabu');
+      $('.navbar').addClass('bg-light');
       $('.navbar').addClass('animate__animated animate__fadeInDown');
     } else {
-      $('.navbar').removeClass('animate__animated animate__fadeInDown');
-      $('.navbar').addClass('animate__animated animate__fadeIn');
+      $('.navbar').addClass('bg-abuabu');
+      $('.navbar').removeClass('bg-light');
+      $('.navbar').removeClass('animate__animated animate__fadeInDown'); // $('.navbar').addClass('animate__animated animate__fadeIn');
     }
 
     if ($(document).scrollTop() > 200) {
       $('.about').addClass('animate__animated animate__fadeInUp');
       $('.tombol-up').html("<a class=\"ignielToTop\" onclick=\"topFunction()\"></a>");
       $('.ignielToTop').addClass('animate__animated animate__fadeInUp');
+    } else {
+      $('.ignielToTop').removeClass('animate__animated animate__fadeInUp');
+      $('.tombol-up').html("");
     }
 
     if ($(document).scrollTop() > 700) {
